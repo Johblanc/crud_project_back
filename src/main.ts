33f34036eb0634cb7ce862153d1c6d1fc7e5e0ed
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-03-20 10:19:57 +0100

    Body : Port change

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
   app.useGlobalInterceptors(new ResponserInterceptor());
 
-  await app.listen(3000);
+  await app.listen(8000);
 }
 bootstrap();
