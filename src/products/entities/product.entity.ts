7d3a2f366d0abfa8d2deba3712d276c50b403ea7
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-03-20 10:10:55 +0100

    Feat : Products Entity

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,24 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity("products")
+export class Product extends BaseEntity {
+
+  @ApiProperty()
+  @PrimaryGeneratedColumn()
+  id : number
+
+
+  @ApiProperty()
+  @Column()
+  nom : string
+
+  @ApiProperty()
+  @Column({type : "money"})
+  prix : number
+
+  @ApiProperty()
+  @Column()
+  quantite : number
+}
