ba70ae9f72f5be5c563af3fced2646229d880fc9
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-03-20 09:43:21 +0100

    Feat : gestion swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './errors/AllExceptionsFilter';
 
@@ -7,6 +9,16 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api/');
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Iconotheque')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
